04e0cfffd3a2a2ef3d911d13d2a1105ace201cdf
Rodger0208 <rodger02081990@gmail.com>
2023-02-05 01:29:42 +0300

    new voids

--- chat/register.h
+++ chat/register.h
@@ -1,14 +1,21 @@
 #pragma once
 #include "container.h"
 #include "user.h"
-
+#include "message.h"
 
 string name;
 string login;
 string password;
 short usersNum = 0;
+int messagesNum = 0;
+string currentUser;
+
+void readMessage();
+void writeMessage();
+void userMenu();
 
 Container<User> users(1);
+Container<Message> messages(1);
 
 void reg()
 {
@@ -31,6 +38,58 @@ void signUp()
 	for (int i = 0; i < users.getSize(); ++i)
 	{
 		if (users.operator[](i).getLogin() == login && users.operator[](i).getPassword() == password)
-			cout << "Login correct" << endl;
+		{
+			cout << "Login correct! Hello " << users[i].getName() << endl;
+			currentUser = login;
+			userMenu();
+		}
+		
 	}
+	throw "incorrect user or password";
+}
+
+
+
+void userMenu()
+{
+	cout << "Press [1] - read message; [2] - write message; [3] - exit" << endl;
+	short ch;
+	cin >> ch;
+	switch (ch)
+	{
+	case 1:
+		readMessage();
+		break;
+	case 2:
+		writeMessage();
+		break;
+	case 3:
+		exit(0);
+		break;
+	default:
+		break;
+	}
+}
+
+void readMessage()
+{
+	for (int i = 0; i < messages.getSize(); i++)
+	{
+		if (messages[i].getTo() == currentUser || messages[i].getTo() == "all")
+			cout << messages[i].getFrom() << ": " << messages[i].getMessage() << endl;
+	}
+	userMenu();
+}
+
+void writeMessage()
+{
+	string to;
+	string sms;
+	cout << "to whom(write 'all' for all): ";
+	cin >> to;
+	cout << "enter message: ";
+	cin >> sms;
+	messages.addItem(Message(currentUser, to, sms));
+	messagesNum++;
+	userMenu();
 }
