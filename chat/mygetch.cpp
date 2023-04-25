1f0ab17faa89786ef36f6680daa54788e7085698
marat-mak <marat.mak@gmail.com>
2023-04-26 03:04:19 +0500

    Final version for Linux

---
+++ chat/mygetch.cpp
@@ -0,0 +1,16 @@
+#include "mygetch.h"
+
+
+int mygetch( ) {
+struct termios oldt,
+newt;
+int ch;
+tcgetattr( STDIN_FILENO, &oldt );
+newt = oldt;
+newt.c_lflag &= ~( ICANON | ECHO );
+tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+ch = getchar();
+tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+return ch;
+}
+
---
+++ chat/mygetch.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+int mygetch( );
